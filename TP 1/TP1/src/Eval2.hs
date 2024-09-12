823cf791e94493e7f1c9f4e47fc7eb83abdecf80
DonBerge <agustinfb93@gmail.com>
2024-09-12 16:51:41 -0300

    Ejercicio 8

--- TP 1/TP1/src/Eval2.hs
+++ TP 1/TP1/src/Eval2.hs
@@ -4,6 +4,7 @@ module Eval2
   )
 where
 
+import Prelude hiding (uncurry)
 import           AST
 import qualified Data.Map.Strict               as M
 import           Data.Strict.Tuple
@@ -11,20 +12,25 @@ import           Data.Strict.Tuple
 -- Estados
 type State = M.Map Variable Int
 
+pair:: a->b->Pair a b
+pair = (:!:)
+
 -- Estado vacío
 -- Completar la definición
 initState :: State
-initState = undefined
+initState = M.empty
 
 -- Busca el valor de una variable en un estado
 -- Completar la definición
 lookfor :: Variable -> State -> Either Error Int
-lookfor = undefined
+lookfor v st = case M.lookup v st of
+                Nothing -> Left UndefVar
+                Just a -> Right a
 
 -- Cambia el valor de una variable en un estado
 -- Completar la definición
 update :: Variable -> Int -> State -> State
-update = undefined
+update = M.insert
 
 -- Evalúa un programa en el estado vacío
 eval :: Comm -> Either Error State
@@ -41,9 +47,75 @@ stepCommStar c    s = do
 -- Evalúa un paso de un comando en un estado dado
 -- Completar la definición
 stepComm :: Comm -> State -> Either Error (Pair Comm State)
-stepComm = undefined
+stepComm (Let v e) st = case evalExp e st of
+                                      Left e -> Left e
+                                      Right e' -> Right $ pair Skip $ uncurry (update v) $ e'
+  
+stepComm (Seq Skip c1) st = stepComm c1 st
+stepComm (Seq c0 c1) st = case stepComm c0 st of
+                            Left e -> Left e
+                            Right (c0' :!: st') -> stepComm (Seq c0' c1) st'
+
+stepComm Skip st = Right $ pair Skip st
+stepComm (IfThenElse b c0 c1) st = case evalExp b st of
+                                    Left e -> Left e
+                                    Right (b' :!: st') -> if b' then Right $ pair c0 st' else Right $ pair c1 st'
+stepComm (RepeatUntil c b) st = stepComm (Seq c (IfThenElse b Skip (RepeatUntil c b))) st
+
+-----------------------------
+
+evalVarOp :: Variable -> (Int->Int) -> State -> Either Error (Pair Int State)
+evalVarOp v op st = case lookfor v st of
+                        Left e -> Left e
+                        Right v' -> let nv = op v'
+                                    in if v' == nv 
+                                       then Right $ pair v' st
+                                       else Right $ pair nv (update v nv st)
+
+evalBinOpWithCheck :: Exp a -> Exp a -> (a->a->b) -> (a->a->Maybe Error) -> State -> Either Error (Pair b State)
+evalBinOpWithCheck e0 e1 op check st = case evalExp e0 st of
+                          Left e -> Left e
+                          Right (n0 :!: st') -> case evalExp e1 st' of
+                                                      Left e -> Left e
+                                                      Right (n1 :!: st'') -> case check n0 n1 of
+                                                                              Nothing -> Right $ pair (op n0 n1) st''
+                                                                              Just e -> Left e
+evalUnOp :: Exp a -> (a->b) -> State -> Either Error (Pair b State)
+evalUnOp e0 op st = case evalExp e0 st of
+                      Left e -> Left e
+                      Right (n0 :!: st') -> Right $ pair (op n0) st'
+
+evalConst :: a -> State -> Either Error (Pair a State)
+evalConst a b = Right $ pair a b
+
+checkDivByZero a b = if b == 0 then Just DivByZero else Nothing
+
+noCheck :: a -> a -> Maybe Error
+noCheck _ _ = Nothing
 
 -- Evalúa una expresión
 -- Completar la definición
 evalExp :: Exp a -> State -> Either Error (Pair a State)
-evalExp = undefined
+evalExp (Const a) = evalConst a
+evalExp (UMinus e) = evalUnOp e negate
+
+evalExp (Var v) = evalVarOp v id
+-- VarInc
+evalExp (VarInc v)= evalVarOp v (+1)
+
+-- VarDec
+evalExp (VarDec v) = evalVarOp v (subtract 1)
+-- BVal
+evalExp BTrue = evalConst True
+evalExp BFalse = evalConst False
+evalExp (Plus e0 e1) = evalBinOpWithCheck e0 e1 (+) noCheck
+evalExp (Minus e0 e1) = evalBinOpWithCheck e0 e1 (-) noCheck
+evalExp (Times e0 e1) = evalBinOpWithCheck e0 e1 (*) noCheck
+evalExp (Div e0 e1) = evalBinOpWithCheck e0 e1 div checkDivByZero
+evalExp (Lt e0 e1) = evalBinOpWithCheck e0 e1 (<) noCheck
+evalExp (Gt e0 e1) = evalBinOpWithCheck e0 e1 (>) noCheck
+evalExp (And e0 e1) = evalBinOpWithCheck e0 e1 (&&) noCheck
+evalExp (Or e0 e1) = evalBinOpWithCheck e0 e1 (||) noCheck
+evalExp (Not e0) = evalUnOp e0 not
+evalExp (Eq e0 e1) = evalBinOpWithCheck e0 e1 (==) noCheck
+evalExp (NEq e0 e1) = evalBinOpWithCheck e0 e1 (/=) noCheck
