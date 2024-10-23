ab5b2b6f03ee6964daa329078ae7c269b386d0d5
DonBerge <agustinfb93@gmail.com>
2024-10-23 02:46:53 -0300

    Infer listo

--- TP2/src/Simplytyped.hs
+++ TP2/src/Simplytyped.hs
@@ -12,7 +12,7 @@ where
 
 import           Data.List
 import           Data.Maybe
-import           Prelude                 hiding ( (>>=) )
+import           Prelude                 hiding ( (>>=), (>>) )
 import           Text.PrettyPrint.HughesPJ      ( render )
 import           PrettyPrinter
 import           Common
@@ -85,9 +85,12 @@ sub i t (Rec u v w) = let
 -- convierte un valor en el término equivalente
 quote :: Value -> Term
 quote (VLam t f) = Lam t f
--- Ejericio 4
+-- Ejercicio 4
 quote (VNum NZero) = Zero
 quote (VNum (NSuc n)) = Suc $ quote $ VNum n
+-- Ejercicio 6
+quote (VList VNil) = Nil
+quote (VList (VCons x xs)) = Cons (quote (VNum x)) (quote (VList xs))
 
 -- evalúa un término en un entorno dado
 eval :: NameEnv Value Type -> Term -> Value
@@ -113,10 +116,24 @@ eval nvs (Suc n) = let
                     (VNum n') = eval nvs n
                    in
                     VNum $ NSuc n'
-eval nvs (Rec t1 t2 t3) = case t3 of
-                            Zero -> eval nvs t1
-                            Suc t -> eval nvs $ t2 :@: Rec t1 t2 t :@: t
-
+eval nvs (Rec t1 t2 t3) = case eval nvs t3 of
+                            VNum NZero -> eval nvs t1
+                            VNum (NSuc t) -> let
+                                                t' = quote $ VNum t
+                                               in eval nvs $ t2 :@: Rec t1 t2 t' :@: t'
+                            VList VNil -> eval nvs t1
+                            VList (VCons n lv) -> let
+                                                    n'  = quote $ VNum n
+                                                    lv' = quote $ VList lv 
+                                                  in
+                                                    eval nvs $ t2 :@: n' :@: lv' :@: Rec t1 t2 lv'
+-- Ejercicio 6
+eval nvs Nil = VList VNil
+eval nvs (Cons x xs) = let
+                        (VNum x') = eval nvs x
+                        (VList xs') = eval nvs x
+                       in
+                        VList $ VCons x' xs'
 
 ----------------------
 --- type checker
@@ -153,34 +170,56 @@ notfunError t1 = err $ render (printType t1) ++ " no puede ser aplicado."
 notfoundError :: Name -> Either String Type
 notfoundError n = err $ show n ++ " no está definida."
 
+-- Funciones definidas por nosotros
+match :: Type -> Either String Type -> Either String Type
+match expected_type e@(Left _) = e
+match expected_type e@(Right t)
+  | expected_type == t = e
+  | otherwise = matchError expected_type t
+
+-- Funciones definidas por nosotros
+checkIsFun :: Either String Type -> Either String Type
+checkIsFun e@(Left _) = e
+checkIsFun e@(Right t) = case t of
+                          FunT t1 t2 -> e
+                          _ -> notfunError t
+
+(>>) :: Either String Type -> Either String Type -> Either String Type
+(>>) v f = v >>= const f
+
+matchMultiple :: [(Type, Either String Type)] -> Either String Type -> Either String Type
+matchMultiple _ e@(Left _) = e
+matchMultiple t1s e@(Right t) = fromMaybe (matchMultipleError t1s t) (lookup t t1s)
+  where
+    matchMultipleError t1s t2 =
+      err
+        $ "se esperaba "
+        ++ intercalate " o " (map (render . printType . fst) t1s)
+        ++ ", pero "
+        ++ render (printType t2)
+        ++ " fue inferido."
+
 -- infiere el tipo de un término a partir de un entorno local de variables y un entorno global
 infer' :: Context -> NameEnv Value Type -> Term -> Either String Type
 infer' c _ (Bound i) = ret (c !! i)
-infer' _ e (Free  n) = case lookup n e of
-  Nothing     -> notfoundError n
-  Just (_, t) -> ret t
-infer' c e (t :@: u) = infer' c e t >>= \tt -> infer' c e u >>= \tu ->
-  case tt of
-    FunT t1 t2 -> if tu == t1 then ret t2 else matchError t1 tu
-    _          -> notfunError tt
+infer' _ e (Free  n) = maybe (notfoundError n) (ret . snd) (lookup n e)
+infer' c e (t :@: u) = checkIsFun (infer' c e t) >>= \(FunT t1 t2) -> match t1 (infer' c e u) >> ret t2
 infer' c e (Lam t u) = infer' (t : c) e u >>= \tu -> ret $ FunT t tu
 -- Ejercicio 3
 infer' c e (Let t u) = infer' c e t >>= \tt -> infer' (tt : c) e u
+
 -- Ejercicio 4
 infer' c e Zero = ret NatT
-infer' c e (Suc t) = infer' c e t >>= \tt ->
-  case tt of
-    NatT -> ret NatT
-    _ -> matchError NatT tt
-infer' c e (Rec t1 t2 t3) = infer' c e t1 >>= \tt1 -> infer' c e t2 >>= \tt2 -> infer' c e t3 >>= \tt3 ->
-  case tt3 of
-    NatT -> let
-              wrongTT2Error = matchError (FunT tt1 (FunT NatT tt1)) tt2
-            in case tt2 of
-              (FunT tu (FunT NatT tv)) -> if tt1 == tu && tt1 == tv
-                                            then ret tt1
-                                            else wrongTT2Error
-              _ -> wrongTT2Error
-    _ -> matchError NatT tt3
-
+infer' c e (Suc t) = match NatT $ infer' c e t
+infer' c e (Rec t1 t2 t3) = 
+  infer' c e t1 >>= \tt1 ->
+    matchMultiple [
+      (FunT tt1 (FunT NatT tt1), match NatT (infer' c e t3)),
+      (FunT NatT (FunT ListT (FunT tt1 tt1)), match ListT (infer' c e t3))
+    ] (infer' c e t2) 
+    >> ret tt1
+-- Ejercicio 6
+infer' c e Nil = ret ListT
+infer' c e (Cons t1 t2) = 
+  match NatT (infer' c e t1) >> match ListT (infer' c e t2) >> ret ListT
 
